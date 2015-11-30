94d1ef16e267ea25d78ea0185c83c6d7858036e4
MetalMichael <metalmichael@hotmail.co.uk>
2015-11-30 15:22:33 +0000

    Refactor Enums

--- SteamAPISession.cs
+++ SteamAPISession.cs
@@ -14,59 +14,6 @@ namespace SteamAPI
     /// </summary>
     public class SteamApiSession
     {
-        /// <summary>
-        /// Available sizes of user avatars.
-        /// </summary>
-        public enum AvatarSize
-        {
-            Small,
-            Medium,
-            Large
-        }
-
-        /// <summary>
-        /// Enumeration of possible authentication results.
-        /// </summary>
-        public enum LoginStatus
-        {
-            LoginFailed,
-            LoginSuccessful,
-            SteamGuard
-        }
-
-        /// <summary>
-        /// Visibility of a user's profile.
-        /// </summary>
-        public enum ProfileVisibility
-        {
-            Private = 1,
-            Public = 3,
-            FriendsOnly = 8
-        }
-
-        /// <summary>
-        /// Available update types.
-        /// </summary>
-        public enum UpdateType
-        {
-            UserUpdate,
-            Message,
-            Emote,
-            TypingNotification
-        }
-
-        /// <summary>
-        /// Status of a user.
-        /// </summary>
-        public enum UserStatus
-        {
-            Offline = 0,
-            Online = 1,
-            Busy = 2,
-            Away = 3,
-            Snooze = 4
-        }
-
         private string _accessToken;
         private int _message;
         private string _steamid;
